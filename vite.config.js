71e6000627196199c1724289cbb00e2aab242378
Matvey Shimorin <mashimorin@edu.hse.ru>
2025-05-17 11:01:41 +0300

    j

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:`/Market-API-React/`
+  plugins: [react({
+    include: '**/*.{js,jsx}' // Обрабатывать оба расширения
+  })],
+  base:`/Market-API-React/`,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
